61fab9b804f263aeb1433f40c08915bcda00c422
Chih-Lin Lee <mlee749@gmail.com>
2019-04-04 10:30:44 -0700

    get request working

--- server/server.js
+++ server/server.js
@@ -6,7 +6,21 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());
-app.use(express.static('view'))
+app.use(express.static('view'));
+
+app.get('/scenarios', (req, res) => {
+  fs.readFile('scenarios.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      const scenarios = []
+      for (let key in JSON.parse(data)) {
+        scenarios.push(key)
+      }
+      res.status(200).send({ scenarios });
+    }
+  })
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
